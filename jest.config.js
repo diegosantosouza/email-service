7908ed6247ffee64ff1f142c6729c4de2ac140fa
diegosantosouza <diegosantos.s@hotmail.com>
2023-10-03 18:23:14 -0300

    feat: test suit

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  roots: ['<rootDir>/tests'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
+  coverageDirectory: 'coverage',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+}
